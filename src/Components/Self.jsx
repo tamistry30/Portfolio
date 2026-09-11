04e33c87d2814e739e8b98479d63f3ee0bf615a4
Tejas Mistry <63935690+tamistry30@users.noreply.github.com>
2026-09-10 21:41:15 -0400

    Update headline and role description in Self component

--- src/Components/Self.jsx
+++ src/Components/Self.jsx
@@ -16,11 +16,11 @@ const Self = () => {
             <div>
                
                 <h2 className="headline-1 max-w-[15ch] sm:max-w-[20ch] lg:max-w-[15ch] mt-5 mb-8 lg:mb-10">
-                Turning complexity into clarity - through data, cloud, and AI.
+                Turning complexity into clarity through data, cloud, and AI.
                 </h2>
 
                 <p className="mb-8 text-lg text-zinc-400 items-center">
-                Data Analytic Engineer | Building at the intersection of Data, Cloud & AI 
+                Data Engineer | Building at the intersection of Data, Cloud & AI 
                 </p>
 
                 <div className="flex items-center gap-3">
